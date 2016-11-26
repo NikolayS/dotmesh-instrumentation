b7af29c1459a5e789897b668a1e7e79401983ae6
Luke Marsden <luke@clusterhq.com>
2016-11-26 14:59:13 +0000

    yuck

--- README.md
+++ README.md
@@ -3,10 +3,24 @@
 ELK stack, Zipkin, and etcd-ui.
 All run in Docker Compose.
 
-To start it, run:
+First, grab some creds for etcd:
 
 ```
-./launch.sh
+mkdir ~/.datamesh && scp user@cluster-node:~/.datamesh/pki ~/.datamesh/
 ```
+
+Start the stack by running:
+
+```
+./launch.sh password
+```
+
+This will make available ports:
+
+- etcd-browser => 81
+- zipkin => 82
+- kibana => 83
+
+All will be vaguely protected by basic auth with username 'admin' and password 'password'.
 
 Suitable for development use.
--- launch.sh
+++ launch.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+PASSWORD=$1
+if [ "$PASSWORD" == "" ]; then
+	echo "Please specify a password as the first argument."
+	exit 1
+fi
+
 (cd docker-elk && docker-compose up -d)
 (cd docker-zipkin && docker-compose up -d)
 (cd etcd-browser && docker build -t etcd-browser . && \
@@ -9,3 +16,18 @@
         -e ETCDCTL_KEY_FILE=~/.datamesh/pki/apiserver-key.pem \
         -e ETCDCTL_CERT_FILE=~/.datamesh/pki/apiserver.pem \
         -t -i etcd-browser)
+
+HTPASSWD=$(docker run --rm -ti --entrypoint htpasswd crosbymichael/htpasswd -nb admin $PASSWORD)
+
+# Ports:
+# - etcd-browser 8000 => 81
+# - zipkin 9411 => 82
+# - kibana 5601 => 83
+
+ARGS1="-e FORWARD_PORT=8000 --link etcd-browser:web       -p 81:80 --name auth-etcd-browser"
+ARGS2="-e FORWARD_PORT=9411 --link zipkin:web             -p 82:80 --name auth-zipkin"
+ARGS3="-e FORWARD_PORT=5601 --link dockerelk_kibana_1:web -p 83:80 --name auth-kibana"
+
+docker run -d -e HTPASSWD="$HTPASSWD" $ARGS1 beevelop/nginx-basic-auth
+docker run -d -e HTPASSWD="$HTPASSWD" $ARGS2 beevelop/nginx-basic-auth
+docker run -d -e HTPASSWD="$HTPASSWD" $ARGS3 beevelop/nginx-basic-auth
