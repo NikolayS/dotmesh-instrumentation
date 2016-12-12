3663e6f0df0679e606787fbd43af0f4d39b88ebe
Luke Marsden <me@lukemarsden.net>
2016-12-12 14:15:45 +0400

    Add support for connecting to local etcd.

--- down.sh
+++ down.sh
@@ -3,5 +3,5 @@
 (cd docker-elk && docker-compose down)
 (cd docker-zipkin && docker-compose down)
 
-docker rm -f etcd-browser
-docker rm -f auth-etcd-browser auth-zipkin auth-kibana
+docker rm -f etcd-browser etcd-browser-local
+docker rm -f auth-etcd-browser auth-zipkin auth-kibana auth-etcd-browser-local
--- up.sh
+++ up.sh
@@ -12,12 +12,23 @@ if [ "$ETCD" == "" ]; then
 	exit 1
 fi
 
+docker rm -f auth-etcd-browser auth-zipkin auth-kibana etcd-browser
+
 (cd docker-elk && docker-compose up -d)
 (cd docker-zipkin && docker-compose up -d)
 (cd etcd-browser && docker build -t etcd-browser . && \
     docker run -d -v /home:/home \
         --name etcd-browser -p 0.0.0.0:8000:8000 \
         --env ETCD_HOST=$ETCD -e ETCD_PORT=42379 \
+        -e ETCDCTL_CA_FILE=~/.datamesh/pki-remote/ca.pem \
+        -e ETCDCTL_KEY_FILE=~/.datamesh/pki-remote/apiserver-key.pem \
+        -e ETCDCTL_CERT_FILE=~/.datamesh/pki-remote/apiserver.pem \
+        -t -i etcd-browser)
+# Local etcd
+(cd etcd-browser && docker build -t etcd-browser . && \
+    docker run -d -v /home:/home \
+        --name etcd-browser-local -p 0.0.0.0:8001:8000 \
+        --env ETCD_HOST=172.17.0.1 -e ETCD_PORT=42379 \
         -e ETCDCTL_CA_FILE=~/.datamesh/pki/ca.pem \
         -e ETCDCTL_KEY_FILE=~/.datamesh/pki/apiserver-key.pem \
         -e ETCDCTL_CERT_FILE=~/.datamesh/pki/apiserver.pem \
@@ -29,11 +40,14 @@ HTPASSWD=$(docker run --rm -ti --entrypoint htpasswd crosbymichael/htpasswd -nb 
 # - etcd-browser 8000 => 81
 # - zipkin 9411 => 82
 # - kibana 5601 => 83
+# - etcd-browser-local 8000 => 84
 
-ARGS1="-e FORWARD_PORT=8000 --link etcd-browser:web -p 81:80 --name auth-etcd-browser"
-ARGS2="-e FORWARD_PORT=9411 --link zipkin:web       -p 82:80 --name auth-zipkin"
-ARGS3="-e FORWARD_PORT=5601 --link kibana:web       -p 83:80 --name auth-kibana"
+ARGS1="-e FORWARD_PORT=8000 --link etcd-browser:web       -p 81:80 --name auth-etcd-browser"
+ARGS2="-e FORWARD_PORT=9411 --link zipkin:web             -p 82:80 --name auth-zipkin"
+ARGS3="-e FORWARD_PORT=5601 --link kibana:web             -p 83:80 --name auth-kibana"
+ARGS4="-e FORWARD_PORT=8001 --link etcd-browser-local:web -p 84:80 --name auth-etcd-browser-local"
 
 docker run -d -e HTPASSWD="$HTPASSWD" $ARGS1 beevelop/nginx-basic-auth
 docker run -d -e HTPASSWD="$HTPASSWD" $ARGS2 beevelop/nginx-basic-auth
 docker run -d -e HTPASSWD="$HTPASSWD" $ARGS3 beevelop/nginx-basic-auth
+docker run -d -e HTPASSWD="$HTPASSWD" $ARGS4 beevelop/nginx-basic-auth
