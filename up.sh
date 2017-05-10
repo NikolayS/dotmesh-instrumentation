bb90894528d979b3f2ae05701c2ebf0880cbb76d
Luke Marsden <me@lukemarsden.net>
2017-05-10 09:19:12 +0200

    We do need a static etcd browser here though.

--- up.sh
+++ up.sh
@@ -6,16 +6,24 @@ if [ "$PASSWORD" == "" ]; then
 	exit 1
 fi
 
-#ETCD=$2
-#if [ "$ETCD" == "" ]; then
-#	echo "Please specify IP of a datamesh cluster node as the second argument."
-#	exit 1
-#fi
+ETCD=$2
+if [ "$ETCD" == "" ]; then
+	echo "Please specify IP of a datamesh cluster node as the second argument."
+	exit 1
+fi
 
 ./down.sh
 
 (cd docker-elk && docker-compose up -d)
 (cd docker-zipkin && docker-compose up -d)
+(cd etcd-browser && docker build -t etcd-browser . && \
+	docker run --restart=always -d -v $HOME:/root \
+		--name etcd-browser -p 0.0.0.0:8000:8000 \
+		--env ETCD_HOST=$ETCD -e ETCD_PORT=42379 \
+		-e ETCDCTL_CA_FILE=/root/.datamesh/pki/ca.pem \
+		-e ETCDCTL_KEY_FILE=/root/.datamesh/pki/apiserver-key.pem \
+		-e ETCDCTL_CERT_FILE=/root/.datamesh/pki/apiserver.pem \
+		-t -i etcd-browser)
 
 HTPASSWD=$(docker run --rm -ti --entrypoint htpasswd crosbymichael/htpasswd -nb admin $PASSWORD)
 
@@ -26,12 +34,12 @@ HTPASSWD=$(docker run --rm -ti --entrypoint htpasswd crosbymichael/htpasswd -nb 
 # - kibana 5601 => 83
 # - etcd-browser-local 8001 => 84
 
-#ARGS1="-e FORWARD_PORT=8000 --link etcd-browser:web       -p 81:80 --name auth-etcd-browser"
+ARGS1="-e FORWARD_PORT=8000 --link etcd-browser:web       -p 81:80 --name auth-etcd-browser"
 ARGS2="-e FORWARD_PORT=9411 --link zipkin:web             -p 82:80 --name auth-zipkin"
 ARGS3="-e FORWARD_PORT=5601 --link kibana:web             -p 83:80 --name auth-kibana"
 #ARGS4="-e FORWARD_PORT=8001 --link etcd-browser-local:web -p 84:80 --name auth-etcd-browser-local"
 
-#docker run --restart=always -d -e HTPASSWD="$HTPASSWD" $ARGS1 beevelop/nginx-basic-auth
+docker run --restart=always -d -e HTPASSWD="$HTPASSWD" $ARGS1 beevelop/nginx-basic-auth
 docker run --restart=always -d -e HTPASSWD="$HTPASSWD" $ARGS2 beevelop/nginx-basic-auth
 docker run --restart=always -d -e HTPASSWD="$HTPASSWD" $ARGS3 beevelop/nginx-basic-auth
 #docker run --restart=always -d -e HTPASSWD="$HTPASSWD" $ARGS4 beevelop/nginx-basic-auth
